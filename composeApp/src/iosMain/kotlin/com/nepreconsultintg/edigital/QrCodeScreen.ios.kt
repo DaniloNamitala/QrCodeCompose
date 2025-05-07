bfea21355ef74510f4ff1c21b72ec10ec743d665
Gustavo César Guedes <gustavo.guedes@nepreconsulting.com>
2025-05-07 16:59:10 -0300

    versao minima funcional

--- composeApp/src/iosMain/kotlin/com/nepreconsultintg/edigital/QrCodeScreen.ios.kt
+++ composeApp/src/iosMain/kotlin/com/nepreconsultintg/edigital/QrCodeScreen.ios.kt
@@ -1,9 +1,9 @@
 package com.nepreconsultintg.edigital
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-
 @Composable
 actual fun CameraScreen(onCodeRead: (code : ByteArray?) -> Unit) {
-
+    Text("Tela do ios")
 }
